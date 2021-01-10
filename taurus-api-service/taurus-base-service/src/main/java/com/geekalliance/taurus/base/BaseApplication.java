6cda7db8552488a413305a3572f6aad150d383c6
MarkBell <markbell.cn@gmail.com>
2021-01-10 22:10:34 +0800

    添加微服务 nacos作为注册中心

--- taurus-api-service/taurus-base-service/src/main/java/com/geekalliance/taurus/base/BaseApplication.java
+++ taurus-api-service/taurus-base-service/src/main/java/com/geekalliance/taurus/base/BaseApplication.java
@@ -2,10 +2,12 @@ package com.geekalliance.taurus.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableAsync
 public class BaseApplication {
     public static void main(String[] args) {
